32903557fccb4f2422dc5073b087347478077d84
tasnim-moutia <84985151+tasnim-moutia@users.noreply.github.com>
2021-06-07 20:32:39 +0100

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -93,7 +93,7 @@ return [
 ],
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'moutiatasnimpt@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', ''),
         'name' => env('MAIL_FROM_NAME', 'LARAVEL'),
     ],
 
